2b39abf3c45c6be76a52aed69b4d213934598898
Code-monkey90 <78129243+Code-monkey90@users.noreply.github.com>
2021-06-25 16:43:47 +0100

    Add files via upload

---
+++ Week 2/Practice/Arrays.js
@@ -0,0 +1,27 @@
+let veg = ["Broccolli", "Onion", "Tomato","Broccolli", "Onion", "Tomato","Broccolli", "Onion", "Tomato","Broccolli", "Onion", "Tomato","Broccolli", "Onion", "Tomato","Broccolli", "Onion", "Tomato","Broccolli", "Onion", "Tomato","Broccolli", "Onion", "Tomato"];
+let fruit = ["Apple", "Pear", "Orange"];
+
+for (let i = 0; i < veg.length; i++){
+console.log(veg[i]);
+console.log(fruit[i]);
+}
+
+let shopping = [
+    ["yogurt", "milk", "apple juice"],
+    ["bananas", "apples", "oranges"],
+    ["rice", "pasta", "noodles"]
+    ]
+    console.log(shopping[1][2]);
+    //output: oranges
+
+    let shopping = [
+        ["Broccolli", "Onion", "Tomato"],
+        ["Apple", "Pear", "Orange"],
+        ["Soda", "Wholemeal", "Regular"]
+        ];
+        
+        
+        console.log(shopping[1][2]);
+        console.log(shopping[0][2]);
+        console.log(shopping[0][1]);
+        console.log(shopping[0][1]);
